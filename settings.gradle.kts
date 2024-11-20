a9bd60148058d4b0c8523d470c203499c4c9f0e9
phinner <62483793+phinner@users.noreply.github.com>
2024-11-20 01:15:27 +0100

    chore(deps): Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,1 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-        maven("https://maven.xpdustry.com/releases") {
-            name = "xpdustry-releases"
-            mavenContent { releasesOnly() }
-        }
-    }
-}
-
 rootProject.name = "flex"
